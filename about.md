26454ebb6c3463dc2875fbc3dd1f4e7c91dc0623
mamamiyear <mamamiyear@163.com>
2017-08-20 23:50:55 +0800

    add ABOUT ME in Chinese.

--- about.md
+++ about.md
@@ -15,3 +15,11 @@ permalink: /about/
 |**WORK**:              &emsp;      |HUAWEI Technology Co., Ltd.|
 
 
+|                                |      |
+| ----                           | ---- |
+|**姓名**:            &emsp;      |钱&emsp;睿|
+|**性别**:            &emsp;      |♂ |
+|**网名**:            &emsp;      |mamamiyear|
+|**学位**:            &emsp;      |工学硕士|
+|**毕业**:            &emsp;      |电子科技大学|
+|**工作**:            &emsp;      |华为技术有限公司|
--- js/index.js
+++ js/index.js
@@ -9,32 +9,31 @@ $(document).ready(function () {
         tag_click_function(this);
     });
 });
+
 prepare_global_settings = function () {
+    //颜色表初始化
     colors_map = {
         "$icon-color": "rgb(127, 127, 127)",
         "$icon-color-selected": "rgb(62, 200, 170)"
     };
-
+    //选中列表初始化
     tag_selected_list = new Set();
-
-
 };
 
 tag_click_function = function (that) {
-
     // 改变颜色
     toggle_tag_btn_color(that);
-
     // 过滤文章
     flush_tag_selected_article();
-
 };
 
 /**
  * 更改tag标签颜色
  */
 toggle_tag_btn_color = function (btn) {
+
     var color_for_tag_btn = $(btn).css("background-color");
+
     if (color_for_tag_btn === colors_map["$icon-color"]) {
         $(btn).css("background-color", colors_map["$icon-color-selected"]);
         tag_selected_list.add($(btn).text());
@@ -44,6 +43,7 @@ toggle_tag_btn_color = function (btn) {
     } else {
         console.error("tag button not have this state.");
     }
+
 };
 
 /**
@@ -52,6 +52,7 @@ toggle_tag_btn_color = function (btn) {
 flush_tag_selected_article = function () {
 
     var articles = $(".post-list li");
+
     if (tag_selected_list.size === 0) {
         articles.css('display', 'block');
         return;
@@ -59,17 +60,11 @@ flush_tag_selected_article = function () {
     articles.css('display', 'none');
     tag_selected_list.forEach(function (item, index, input) {
         //匿名函数参数第三项为原数组
-        // console.info("tag item:" + item.toString());
         articles.each(function () {
-            // console.info("this li class:" + this.getAttribute("class"));
-            // if ($(this).hasClass(item.toString())){
             var a = item.toString();
             if ($(this).hasClass(a)) {
-                // console.info(this.getAttribute("class"));
                 $(this).css('display', 'block');
             }
         });
     });
-
-
 };
